2514023e028d8dccfce2739a0080fdbdbfd2b0d2
Takimi-no <takuyasakura@icloud.com>
2026-04-19 15:42:11 +0000

    add：new define to make input_text to model

--- chat_terminal.py
+++ chat_terminal.py
@@ -8,3 +8,15 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 # ベースモデルにLoRAを重ねるために利用
 from peft import PeftModel
 
+DEFAULT_BASE_MODEL = "llm-jp/llm-jp-3-13b-instruct3"
+
+# history：会話履歴
+# user_message：今回の入力
+def build_input(history, user_message):
+    parts = []
+    for user,assistant in history:
+        parts.append(f"User: {user}\nAssistant: {assistant}")
+    parts.append(f"User: {user_message}\nAssistant:")
+    # 複数の文字列(User:〜〜Assistant:〜〜)を\nを間に挟むことで1つの文字列にまとめている。
+    return "\n".join(parts)
+        
